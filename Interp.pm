7099db4da40c521620c2c67edd9f0911b31193d0
jima <jima>
2020-05-14 06:07:31 +0000

    #

--- Interp.pm
+++ Interp.pm
@@ -8,7 +8,7 @@ use strict; use warnings FATAL => 'all'; use 5.010;
 # (Perl assumes Latin-1 by default).
 use utf8;
 
-$Vis::VERSION = sprintf "%d.%03d", q$Revision: 1.125 $ =~ /(\d+)/g;
+$Vis::VERSION = sprintf "%d.%03d", q$Revision: 1.126 $ =~ /(\d+)/g;
 
 # Copyright © Jim Avera 2012-2014.  Released into the Public Domain
 # by the copyright owner.  (jim.avera AT gmail dot com)
@@ -904,8 +904,8 @@ sub forceqsh($) {
   }
 }
 
-sub qsh(_;@) {  # N.B. "_" prototype defaults to $_
-  my @args = @_;  # needed in case $_ is being used
+sub qsh(_;@) {
+  my @args = @_;  # needed in case $_ is bound by prototype "_"
   my @results = map {
                   defined $_
                     ? (/[^-=\w_\/:\.,]/ || $_ eq "" || ref) ? forceqsh($_) : $_
