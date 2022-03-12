40447fb3b1152c83b923085ae9d2c178356f040a
Jim Avera <jim.avera@gmail.com>
2022-03-11 17:42:44 -0800

    initial commit

---
+++ lib/Data/Dumper/Interp.pm
@@ -0,0 +1,39 @@
+package Data::Dumper::Interp;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Data::Dumper::Interp - It's new $module
+
+=head1 SYNOPSIS
+
+    use Data::Dumper::Interp;
+
+=head1 DESCRIPTION
+
+Data::Dumper::Interp is ...
+
+=head1 LICENSE
+
+Copyright (C) Jim Avera.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Jim Avera E<lt>jim.avera@gmail.comE<gt>
+
+=cut
+
